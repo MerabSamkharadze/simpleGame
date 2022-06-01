c3a541f5cd784a70fda61824a05e29e177c1d32f
Mebo <100339882+MerabSamkharadze@users.noreply.github.com>
2022-06-01 19:29:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function main(currentTime) {
     document.querySelector(".score1").textContent = 0;
 
     if (confirm("ვუფს... დარჩი მშიერი 🥺 ")) {
-      location.reload();
+      window.location.reload();
     }
     return;
   }
